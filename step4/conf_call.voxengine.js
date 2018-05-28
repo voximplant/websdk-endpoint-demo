81604bc4bb40f692a3b74785cc094f264070ceca
Igor Sheko <i@irbisadm.ru>
2018-05-28 13:38:01 +0300

    Introduce the "callConference" function.

--- step4/conf_call.voxengine.js
+++
@@ -1,8 +0,0 @@
-VoxEngine.addEventListener(AppEvents.CallAlerting, function(e) {
-  const call = VoxEngine.callConference("myconf",
-    e.callerid,
-    e.displayName,
-    {},
-    e.scheme);
-  VoxEngine.easyProcess(e.call, call);
-});
